29de223cf39ef5023b5a9e80ad047a07a6f91a24
Andres Osorio <andres150291@gmail.com>
2020-04-18 17:53:06 -0400

    Add .env file

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// Import dotenv;
+// Dotenv is a zero-dependency module that loads 
+// environment variables from a .env file into process.env. 
+// Storing configuration in the environment separate from code 
+// is based on The Twelve-Factor App methodology.
+require('dotenv').config();
+
 const express = require('express');
 
 // Get the routes from the ./routes/index.js
